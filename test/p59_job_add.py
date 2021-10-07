a5d0d115d6a7b0231f69aede3ae744cc7383a0ee
BroOkuda <okudam@byui.edu>
2021-10-07 09:28:21 -0600

    working! (sometimes...)

--- test/p59_job_add.py
+++ test/p59_job_add.py
@@ -1,3 +1,7 @@
+#
+# Adds an empty job card to the employer portal
+#
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -40,15 +44,69 @@ print('test start')
 
 login(driver)
 
-createPitchCard(driver)
+# first, count the number of job cards on hand by going to the employee portal
+profile(driver)
+employeePortal(driver)
 
+time.sleep(5)
+
+# get all the rows in the employer portal table (assume only one table exists on the page)
+jobTable = driver.find_elements_by_tag_name('tr')
+
+# count the number of rows; -1 is needed to account for the header row
+num_rows = len(jobTable) - 1
+print('The original number of job cards:', num_rows)
+
+#for row in jobTable:
+#    print(row.text)
+#    print(row.get_attribute('id'))
+
+# create a new job pitch card
+createPitchCard(driver)
 selectJob(driver)
 
-numOfJobCardsToCreate(driver)
 
-sys.exit()
+# Click on the "ADDPITCHCARDS" button in "How many Job PitchCards would you like 
+# to create now?" window. 
+time.sleep(5)
+numCards = driver.find_element_by_xpath(
+   '/html/body/div[1]/div/div[2]/div/app-create-team-pitchcard/div/div[1]/div[2]/div[1]/p-inputnumber/span/input'
+)
 
-logout()
+# some debug info
+#print('element', numCards)
+#print('type', numCards.get_attribute("type"))
+#print('id', numCards.get_attribute("id"))
+#print('aria-valuenow', numCards.get_attribute("aria-valuenow"))
+#print('aria-valumin', numCards.get_attribute("aria-valumin"))
+#print('aria-valuemax', numCards.get_attribute("aria-valuemax"))
+   
+# clear the number of job cards to create.  This will reset to 1.
+numCards.clear()
+   
+# click the add pitchcards button
+addPitchCards = driver.find_element_by_xpath(
+   '/html/body/div[1]/div/div[2]/div/app-create-team-pitchcard/div/div[2]/button'
+)
+addPitchCards.click()
+time.sleep(2)
+print('click add pitchcards')
+
+
+# now, count the number of jopb cards.  It should be one greater.
+# get all the rows in the table (assume only one table exists on the page)
+jobTableNew = driver.find_elements_by_tag_name('tr')
+# count the number of rows; -1 is needed to account for the header row
+num_rows_new = len(jobTableNew) - 1
+print('The number of job cards after additon(s):', num_rows_new)
+
+if num_rows_new != (num_rows + 1):
+   print('test failed.  table row did not inclement by one after adding a job card')
+else:
+   print('success')
+
+
+logout(driver)
 
 driver.back()
 # time.sleep(5)
