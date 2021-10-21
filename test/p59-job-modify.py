f714a9cc042bf2a0bc74d98de193db60cc243465
Nsimbi-ingwe <sch11064@byui.edu>
2021-10-21 11:50:05 -0600

    Update

--- test/p59-job-modify.py
+++ test/p59-job-modify.py
@@ -50,18 +50,13 @@ employeePortal(driver)
 
 time.sleep(5)
 
-#Enters the employer portal
-WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#pr_id_9 > div > div > div > div > div.ui-carousel-item.ui-carousel-item-active.ui-carousel-item-start.ng-star-inserted > div"))).click()
-print('click employer portal')  
-
-
 ########Clicks on the edit button
-WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/main/app-history-favorites-layout/div/div/div/div/div/div[2]/app-account-employer-portal/div[1]/div/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div/img"))).click()
+WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "/html/body/app-root/main/app-history-favorites-layout/div/div/div/div/div/div[2]/app-account-employer-portal/div[1]/div/div/div[1]/div[1]/div[1]/div[2]/div[1]/div/div"))).click()
 print('click on edit button')
 
 
 
-#######Clicks on the billing button. Will delete information and reenter it.
+#######Clicks on the billing button. 
 WebDriverWait(driver, 15).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[2]/app-ep-layout/div[2]/div/div/div[1]/app-stepper/div/div/p-carousel/div/div/div/div/div/div[1]/div"))).click()
 print('click on billing button')
 
@@ -84,7 +79,7 @@ driver.find_element_by_id("title").send_keys('John Brown')
 #delete the email and reenter "jbrown@gmail.com"
 time.sleep(3)
 driver.find_element_by_id("email").clear() 
-time.sleep(1)
+time.sleep(3)
 driver.find_element_by_id("email").send_keys('jbrown@gmail.com')
 
 #delete the phone number and reenter "(123) 456-7890"
@@ -103,8 +98,8 @@ time.sleep(1)
 driver.find_element_by_id("address").send_keys('1009 Larch Drive')
 
 #delete the city and reenter "Rexburg"
-driver.find_element_by_id("/html/body/div[1]/div/div[2]/app-ep-layout/div[2]/div/div/div[2]/div/div/app-enter-information/form/div/div[2]/span[3]/p-autocomplete/span/input").clear()
-driver.find_element_by_id("/html/body/div[1]/div/div[2]/app-ep-layout/div[2]/div/div/div[2]/div/div/app-enter-information/form/div/div[2]/span[3]/p-autocomplete/span/input").send_keys('Rexburg')
+driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/app-ep-layout/div[2]/div/div/div[2]/div/div/app-enter-information/form/div/div[2]/span[3]/p-autocomplete/span/input").clear()
+driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/app-ep-layout/div[2]/div/div/div[2]/div/div/app-enter-information/form/div/div[2]/span[3]/p-autocomplete/span/input").send_keys('Rexburg')
 
 # #delete the state and reenter "ID"
 # driver.find_element_by_id("").clear()
